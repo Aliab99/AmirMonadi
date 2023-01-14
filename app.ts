af32d6fa5ef4207d2cf6cf24313c3a42bbac08e7
aliab99 <ali.ab.99.202@gmail.com>
2023-01-14 14:26:39 +0330

    [BASE]-Initial commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import dotEnv  from 'dotEnv';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import errors from './components/errors/errors';
+
+const app = express();
+dotEnv.config({path:'./config/config.env'});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(errors);
+
+export default app;
